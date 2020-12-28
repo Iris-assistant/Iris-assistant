a478a901f7331ff6e9a5a88e4379659cf0f071f1
Sajanader <sajanader93@gmail.com>
2020-12-28 17:01:09 +0200

    saja

---
+++ book_feature.py
@@ -0,0 +1,18 @@
+import speech_text as st
+from tkinter.filedialog import *
+import PyPDF2
+import pyttsx3
+
+
+
+def read_book():
+    st.respond("Here you go to book shelf\n")
+    book = askopenfilename()
+    reader = PyPDF2.PdfFileReader(book)
+    pages = reader.numPages
+    for num in range(0, pages):
+        page = reader.getPage(num)
+        text = page.extractText()
+        player = pyttsx3.init()
+        player.say(text)
+        player.runAndWait()
